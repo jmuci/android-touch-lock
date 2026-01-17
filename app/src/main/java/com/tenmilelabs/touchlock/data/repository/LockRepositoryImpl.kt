a10ec62d7c22d0125130b13f14439570937efe84
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2026-01-17 07:18:26 -0700

    Add remo impl

--- app/src/main/java/com/tenmilelabs/touchlock/data/repository/LockRepositoryImpl.kt
+++ app/src/main/java/com/tenmilelabs/touchlock/data/repository/LockRepositoryImpl.kt
@@ -1,25 +1,36 @@
 package com.tenmilelabs.touchlock.data.repository
 
 import android.content.Context
+import android.content.Intent
+import androidx.core.content.ContextCompat
 import com.tenmilelabs.touchlock.domain.model.LockState
 import com.tenmilelabs.touchlock.domain.repository.LockRepository
+import com.tenmilelabs.touchlock.service.LockOverlayService
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class LockRepositoryImpl @Inject constructor(
-    private val context: Context
+    @param:ApplicationContext private val context: Context
 ) : LockRepository {
 
     override fun startLock() {
-       // LockOverlayService.start(context)
+        val intent = Intent(context, LockOverlayService::class.java).apply {
+            action = LockOverlayService.ACTION_START
+        }
+        ContextCompat.startForegroundService(context, intent)
     }
 
     override fun stopLock() {
-      //  LockOverlayService.stop(context)
+        val intent = Intent(context, LockOverlayService::class.java).apply {
+            action = LockOverlayService.ACTION_STOP
+        }
+        context.startService(intent)
     }
 
     override fun observeLockState(): Flow<LockState> {
-      //  return LockOverlayService.lockState
-        return TODO("Not yet implemented")
+        return LockOverlayService.lockState
     }
 }
