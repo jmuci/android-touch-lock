5df8ff91aed4cf7fb3c3b20880a8e83c04b569fe
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2026-01-17 07:26:50 -0700

    Overlay Permission UX

---
+++ app/src/main/java/com/tenmilelabs/touchlock/data/permission/OverlayPermissionManager.kt
@@ -0,0 +1,35 @@
+package com.tenmilelabs.touchlock.data.permission
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+import androidx.core.net.toUri
+
+@Singleton
+class OverlayPermissionManager @Inject constructor(
+    @param:ApplicationContext private val context: Context
+) {
+
+    fun hasPermission(): Boolean {
+        return Settings.canDrawOverlays(context)
+    }
+
+    fun createSettingsIntent(): Intent {
+        return Intent(
+            Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+            "package:${context.packageName}".toUri()
+        ).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+    }
+
+    fun requirePermission(context: Context) {
+        if (!hasPermission()) {
+            context.startActivity(createSettingsIntent())
+        }
+    }
+}
