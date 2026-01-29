d3c239e08f924e960701ee18dbe81450214507aa
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2026-01-29 14:11:35 -0700

    Save debug config and add use cases

--- app/src/main/java/com/tenmilelabs/touchlock/domain/repository/ConfigRepository.kt
+++ app/src/main/java/com/tenmilelabs/touchlock/domain/repository/ConfigRepository.kt
@@ -6,4 +6,8 @@ import kotlinx.coroutines.flow.Flow
 interface ConfigRepository {
     fun observeOrientationMode(): Flow<OrientationMode>
     suspend fun setOrientationMode(mode: OrientationMode)
+    
+    // Debug-only: For diagnosing overlay lifecycle issues
+    fun observeDebugOverlayVisible(): Flow<Boolean>
+    suspend fun setDebugOverlayVisible(visible: Boolean)
 }
