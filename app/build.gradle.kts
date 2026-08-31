be95d090f0b3cad5e6a6db2b0abb1d5367593e2d
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2026-08-30 22:45:57 -0600

    Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         applicationId = "com.tenmilelabs.touchlock"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
